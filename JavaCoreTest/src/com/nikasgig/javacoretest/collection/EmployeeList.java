ac46657dcb42d7e0f852c78c12a138f2e1a5ac37
Нікіта Скворцов <Nikita.Skvorsov@cs.khpi.edu.ua>
2022-12-18 15:06:01 +0200

    added Constructor and toString()

--- JavaCoreTest/src/com/nikasgig/javacoretest/collection/EmployeeList.java
+++ JavaCoreTest/src/com/nikasgig/javacoretest/collection/EmployeeList.java
@@ -2,4 +2,21 @@ package com.nikasgig.javacoretest.collection;
 
 public class EmployeeList {
 
+	private int id;
+	private String name;
+	private int salary;
+	public EmployeeList(int id, String name, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeList [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+	
+	
 }
