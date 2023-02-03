30dc0b5b8182eac1f24f71896704c1d5c2ad3bcc
Нікіта Скворцов <Nikita.Skvorsov@cs.khpi.edu.ua>
2023-02-03 15:46:13 +0200

    added test of method count

--- JavaCoreTest/src/com/nikasgig/javacoretest/streams/StreamsMain.java
+++ JavaCoreTest/src/com/nikasgig/javacoretest/streams/StreamsMain.java
@@ -224,14 +224,16 @@ public class StreamsMain {
 		stream12.forEach(e->System.out.print(e + " "));
 		//stream1.close();
 		//stream2.close();
+		System.out.println();
 		System.out.println("----------------------");
 		System.out.println("distinct");
 		Stream<Integer> stream3 = Stream.of(1,3,3,6,6,7,8,10,4,3,2,1,1,6,78,4,3,9,0);
 		stream3.distinct().forEach(e->System.out.print(e + " "));
-		
-		
-		
-		
+		System.out.println();
+		System.out.println("----------------------");
+		System.out.println("count");
+		Stream<Integer> stream4 = Stream.of(1,3,3,6,6,7,8,10,4,3,2,1,1,6,78,4,3,9,0);
+		System.out.println(stream4.count());
 		
 		
 		
