33cb05c1bf0aa85b53ef46e65a1749a76b1bebf4
Нікіта Скворцов <Nikita.Skvorsov@cs.khpi.edu.ua>
2023-02-05 21:30:05 +0200

    added test of Toolkit.getDefaultToolkit().beep(); threads in lambda

--- JavaCoreTest/src/com/nikasgig/javacoretest/multithreading/MultithreadingMain.java
+++ JavaCoreTest/src/com/nikasgig/javacoretest/multithreading/MultithreadingMain.java
@@ -1,5 +1,8 @@
 package com.nikasgig.javacoretest.multithreading;
 
+import java.awt.Toolkit;
+import java.util.concurrent.TimeUnit;
+
 public class MultithreadingMain {
 
 	public void main() {
@@ -26,12 +29,35 @@ public class MultithreadingMain {
 				t.start();
 				Thread t2 = new Thread(new Thread2());
 				t2.start();
+				for (int i = 0; i < 1000; i++) {
+					System.out.print("_");
+				}
+				Toolkit.getDefaultToolkit().beep(); /////////////////////////////////////////////// beep()
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("----------------------");
+				System.out.println("----------------------");
+				System.out.println("Create code thread now");
+				new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						for (int i = 0; i < 100; i++) {
+							System.out.print(1);
+						}
+						
+					}
+					}).start();
 				
-				
-				
-				
-				
-				
+				new Thread(()->{for (int i = 0; i < 100; i++) {System.out.print(2);}}).start();
+				System.out.println("----------------------");
+				System.out.println("----------------------");
+				Toolkit.getDefaultToolkit().beep(); /////////////////////////////////////////////// beep()
+				System.out.println();
 				
 				
 				
